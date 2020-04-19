d5d940003066b9a9d5627877a83c303b80213366
uchaaa <63946511+uchaaa@users.noreply.github.com>
2020-04-19 14:00:50 +0700

    Add files via upload

---
+++ tubesucajots/artis.h
@@ -0,0 +1,6 @@
+#ifndef ARTIS_H_INCLUDED
+#define ARTIS_H_INCLUDED
+
+
+
+#endif // ARTIS_H_INCLUDED
---
+++ tubesucajots/pelem.h
@@ -0,0 +1,6 @@
+#ifndef PELEM_H_INCLUDED
+#define PELEM_H_INCLUDED
+
+
+
+#endif // PELEM_H_INCLUDED
