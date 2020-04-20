b9fd2daaa95a1e4bda573d312e4612929f09631c
uchaaa <63946511+uchaaa@users.noreply.github.com>
2020-04-20 22:03:27 +0700

    Update relasi.cpp

--- tubesucajots/relasi.cpp
+++ tubesucajots/relasi.cpp
@@ -1,1 +1,43 @@
 #include "relasi.h"
+#include "pelem.h"
+#include "artis.h"
+
+adrR createElmR(adrfilm P, adrartis Q){
+
+}
+
+void createListRelasi(listRelasi &L){
+    firstR(R) = NIL;
+    lastR(R) = NIL;
+}
+
+void insertlastRelasi(listRelasi &L, adrR R){
+    if (firstR(L) == NIL && lastR(L) == NIL){
+        firstR(L) = R;
+        lastR(L) = R;
+        nextR(R) = firstR(L);
+    } else{
+        nextR(lastR(L)) = R;
+        nextfilm(R) = firstR(L);
+        lastfilm(L) = R;
+    }
+}
+}
+
+void printRelasi(listRelasi L){
+
+}
+
+adrR cariRelasi(listRelasi L, string u){
+    adrfilm Q;
+
+    Q = firstR(L);
+    while (Q != lastR(L) && infofilm(Q).film != u){
+        Q = nextR(Q);
+    }
+    if (infofilm(Q).film == u){
+        return Q;
+    } else{
+        return NIL;
+    }
+}
