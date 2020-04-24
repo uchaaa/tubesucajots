6f439457b1141d3da14d56d010a5eb33d198fefa
uchaaa <63946511+uchaaa@users.noreply.github.com>
2020-04-24 20:31:25 +0700

    Update relasi.h

--- tubesucajots/relasi.h
+++ tubesucajots/relasi.h
@@ -3,6 +3,7 @@
 #include "pelem.h"
 #include "artis.h"
 #include <iostream>
+#include <stdlib.h>
 
 #define infoRfilm(R) R->infoRfilm
 #define infoRartis(R) R->infoRartis
@@ -30,6 +31,13 @@ adrR createElmR(adrfilm P, adrartis Q);
 void createListRelasi(listRelasi &L);
 void insertlastRelasi(listRelasi &L, adrR R);
 void printRelasi(listRelasi L);
+void inputrelasi(listRelasi &lr,listFilm lp,listArtis lc);
+void deleteelmtrelasiFilm(listRelasi &l,listFilm lp,adrR &p,string film);
+void deleteelmtrelasi(listRelasi &l,adrR &p,string artis,string film);
+void deleteelmtrelasiArtis(listRelasi &l,listArtis lc,adrR &p,string film);
+void tampildatafilmtertentu(listRelasi l,string film);
+void tampildataartistertentu(listRelasi l,string artis);
+void jmlhtampil (listRelasi L,listArtis lc );
 adrR cariRelasi(listRelasi L, string u);
 
 #endif // RELASI_H_INCLUDED
